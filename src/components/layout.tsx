b988b7ff39169760106e781c403da6ea350c8d90
Michael Hampson <michael_hampson@harvard.edu>
2023-05-07 14:53:42 -0400

    better bootstrap fix

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
-import "bootstrap/dist/js/bootstrap.bundle.min";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
