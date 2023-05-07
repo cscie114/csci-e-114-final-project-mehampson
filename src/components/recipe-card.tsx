0d8cdfe9b7189c7437ced65b24a60d1bfe177685
Michael Hampson <michael_hampson@harvard.edu>
2023-05-07 09:56:20 -0400

    Cards fully link to page

--- src/components/recipe-card.tsx
+++ src/components/recipe-card.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Link } from 'gatsby';
 import { Recipe } from '../mealie.d';
 import { recipeCard } from "../components/css/recipes.module.css";
-import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 
@@ -16,13 +16,12 @@ const RecipeCard = ({ recipe }: RecipeCardProps) => {
     const image = getImage(recipe.recipeImg);
     return (
         <Card border="secondary" className={recipeCard}>
-            {image && (<GatsbyImage image={image} alt={recipe.name} className={'card-img-top'} />)}
             <Link to={`recipes/${recipe.slug}`}>
+                {image && (<GatsbyImage image={image} alt={recipe.name} className={'card-img-top'} />)}
                 <Card.Body>
                     <Card.Title>{recipe.name}</Card.Title>
                 </Card.Body>
             </Link>
-
         </Card >
     );
 }
